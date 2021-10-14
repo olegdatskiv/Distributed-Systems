8abc50975c70d7f33f4aa9c6e3ca0b5184f315e6
Yevhen Sapolovych <e.sapolovych@gmail.com>
2021-10-14 15:24:31 +0300

    "Added post method"

--- server_main.py
+++ server_main.py
@@ -1,28 +1,43 @@
-from typing import Any, List, Optional
-
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Message(BaseModel):
+    id: int
+    text: str
+    additional: Optional[str] = None
 
-class Logger:
-    def __init__(self, msgs: List[str] = None):
-        if msgs is None:
-            self.msgs = []
-        else:
-            self.msgs = msgs
-    
-    def add_msg(self, msg: str) -> bool:
-        self.msgs.append(msg)
-        return True
+message_store = {} #created messages will be stored in a dict
 
-    def list_msgs(self):
-        return self.msgs
-
-LOGGER = Logger()
 
 @app.get("/")
-def list_msgs():
-    return LOGGER.list_msgs()
+async def get_main():
+    return "This is a test"
 
-# if __name__ == '__main__':
+# a method to get message by its id
+@app.get("/messages/{message_id}")
+async def read_message(message_id: int):
+    if message_id in message_store:
+        return message_store[message_id]
+    else:
+        return "Message with this id does not exist"
+
+# a method to get an id of a latest message (to assign a new id to a new message)
+@app.get("/messages/last_message_id")
+async def get_last_message_ind(m_store = message_store):
+    # get the len of our message storage 
+    return len(m_store)-1
+    # return "Whoops"
+
+# post a message
+@app.post("/messages/{message_id}")
+async def create_message(message_id: int, message_text: str, message: Message, message_store = message_store):
+    message_dict = message.dict()
+    message_dict['id'] = message_id
+    message_dict['text'] = message_text
+
+    message_store.update({message_id:message_dict}) #does not work like that - why?
+    
+    return f'Successfully created a message\n {message_dict}'
