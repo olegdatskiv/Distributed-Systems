a3f551ada5c356634214a4011684893e5aae40ce
olehdatskiv <oleh.datskiv@globallogic.com>
2021-10-15 15:29:10 +0300

    add Dockerfiles and base implement Secondary node

---
+++ secondary_server_v1/secondary_main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Message(BaseModel):
+    id: int
+    text: str
+
+
+class Secondary:
+    def __init__(self):
+        self._log = {}
+
+    def append_msg(self, msg: Message):
+        self._log[msg.id] = msg.text
+
+    def list_msgs(self):
+        return list(self._log.values())
+
+
+SECONDARY = Secondary()
+
+
+@app.get("/")
+async def get_main():
+    return "This is a secondary node"
+
+
+@app.post('/append_msg')
+async def append_msg(msg: Message):
+    print('im here!')
+    SECONDARY.append_msg(msg)
+    return 'ok'
+
+
+@app.get('/list_msgs')
+async def list_msgs():
+    return SECONDARY.list_msgs()
