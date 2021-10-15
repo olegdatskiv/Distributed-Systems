848624e3afc2a56a6ca80daae884b1bf5fec5dee
olehdatskiv <oleh.datskiv@globallogic.com>
2021-10-15 16:05:12 +0300

    edit host and add post to secondary

--- master_server/master_main.py
+++ master_server/master_main.py
@@ -1,20 +1,32 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
+import requests
 
 app = FastAPI()
+
 
 class Message(BaseModel):
     text: str
 
+
 class MessageId(BaseModel):
     message_id: int
 
-MESSAGE_STORE = {} #created messages will be stored in a dict
+
+class MessageToNode(BaseModel):
+    id: int
+    text: str
+
+
+MESSAGE_STORE = {}  # created messages will be stored in a dict
+
 
 class Master:
-    def __init__(self):
+    def __init__(self, host_node_v1: str = 'http://127.0.0.2:8000', host_node_v2: str = 'http://127.0.0.3:8000'):
         self._log = {}
+        self._host_node_v1 = host_node_v1
+        self._host_node_v2 = host_node_v2
 
     def get_last_message_id(self):
         if len(self._log) == 0:
@@ -24,15 +36,23 @@ class Master:
     def append_msg(self, msg: str):
         prev_id = self.get_last_message_id()
         self._log[prev_id + 1] = msg
+        data = MessageToNode()
+        data.id = prev_id + 1
+        data.text = msg
+        requests.post(self._host_node_v1, data)
+        requests.post(self._host_node_v2, data)
 
     def list_msgs(self):
         return list(self._log.values())
 
+
 MASTER = Master()
+
 
 @app.get("/")
 async def get_main():
     return "This is a master node"
+
 
 @app.post('/append_msg')
 async def append_msg(msg: Message):
@@ -40,9 +60,11 @@ async def append_msg(msg: Message):
     MASTER.append_msg(msg.text)
     return 'ok'
 
+
 @app.get('/list_msgs')
 async def list_msgs():
     return MASTER.list_msgs()
+
 
 # a method to get message by its id
 @app.get("/messages/message/{message_id}")
@@ -52,13 +74,15 @@ async def read_message(message_id: int):
     else:
         return "Message with this id does not exist"
 
+
 # a method to get an id of a latest message (to assign a new id to a new message)
 @app.get("/messages/last_message_id")
 async def get_last_message_ind() -> int:
     # get the len of our message storage 
     print('last_message_id called, returns:', len(MESSAGE_STORE) - 1)
-    return len(MESSAGE_STORE)-1
+    return len(MESSAGE_STORE) - 1
     # return "Whoops"
+
 
 # post a message
 @app.post("/messages/{message_id}")
@@ -67,6 +91,6 @@ async def create_message(message_id: int, message_text: str):
     message_dict['id'] = message_id
     message_dict['text'] = message_text
 
-    MESSAGE_STORE.update({message_id:message_dict}) #does not work like that - why?
+    MESSAGE_STORE.update({message_id: message_dict})  # does not work like that - why?
 
     return f'Successfully created a message\n {message_dict}'
